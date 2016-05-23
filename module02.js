8134b0179fdabc2aeff10a6fc3792a09f0983096
wangzhen <376086383@qq.com>
2016-05-23 10:21:07 +0800

    上传AngularJS文件
    AngularJS

---
+++ module02.js
@@ -0,0 +1,13 @@
+
+
+var app2 = angular.module("myApp2", []);
+
+app2.controller('secondController',function($scope){
+    $scope.name='王振';
+
+});
+
+app2.controller('threeController',function($scope){
+    $scope.name='Brave';
+
+});
